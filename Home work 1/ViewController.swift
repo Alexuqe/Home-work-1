20eb2d2fcab85143c5437806645577b697afdf68
alex <hordiah1@google.com>
2024-08-25 13:29:27 +0300

    Initial Commit

---
+++ Home work 1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Home work 1
+//
+//  Created by Sasha on 25.08.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
